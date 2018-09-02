7e8a1c847aba3db04b8442de4ad34b19e42f9f10
Greg Lhotellier <glhotellier@gmail.com>
2018-09-02 12:03:15 -0400

    CarList

---
+++ View/Models/CarList.swift
@@ -0,0 +1,19 @@
+//
+//  CarList.swift
+//  View
+//
+//  Created by Grégoire Lhotellier on 02/09/2018.
+//  Copyright © 2018 Grégoire Lhotellier. All rights reserved.
+//
+
+public struct CarList {
+
+    public let brand: String
+    public let model: String
+
+    public init(brand: String, model: String) {
+        self.brand = brand
+        self.model = model
+    }
+
+}
