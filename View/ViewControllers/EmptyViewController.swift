4404ec4f59c7f24033f7ec8e65921322aeafb38e
Greg Lhotellier <glhotellier@gmail.com>
2018-09-02 12:01:37 -0400

    EmptyViewController

---
+++ View/ViewControllers/EmptyViewController.swift
@@ -0,0 +1,34 @@
+//
+//  EmptyViewController.swift
+//  View
+//
+//  Created by Grégoire Lhotellier on 02/09/2018.
+//  Copyright © 2018 Grégoire Lhotellier. All rights reserved.
+//
+
+import UIKit
+
+public final class EmptyViewController: UIViewController {
+
+    private let label = UILabel()
+
+    public init(emptyMessage: String) {
+        label.text = emptyMessage
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            ])
+    }
+
+}
