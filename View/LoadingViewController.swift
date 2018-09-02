b500c28354f8080b97ccd7ee108c11a810399f19
Greg Lhotellier <glhotellier@gmail.com>
2018-09-02 11:55:17 -0400

    LoadingViewController

---
+++ View/LoadingViewController.swift
@@ -0,0 +1,33 @@
+//
+//  LoadingViewController.swift
+//  View
+//
+//  Created by Grégoire Lhotellier on 02/09/2018.
+//  Copyright © 2018 Grégoire Lhotellier. All rights reserved.
+//
+
+import UIKit
+
+public final class LoadingViewController: UIViewController {
+
+    private let activityIndicatorView = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        activityIndicatorView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(activityIndicatorView)
+        NSLayoutConstraint.activate([
+            activityIndicatorView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            activityIndicatorView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            ])
+    }
+
+}
