4e2ea7016b896282acf61be67edda31f870f5a96
Greg Lhotellier <glhotellier@gmail.com>
2018-09-02 11:57:53 -0400

    ErrorViewController

---
+++ View/ErrorViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ErrorViewController.swift
+//  View
+//
+//  Created by Grégoire Lhotellier on 02/09/2018.
+//  Copyright © 2018 Grégoire Lhotellier. All rights reserved.
+//
+
+import UIKit
+
+public final class ErrorViewController: UIViewController {
+
+    private let label = UILabel()
+
+    public init(errorMessage: String) {
+        label.text = errorMessage
+        label.textColor = .red
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            ])
+    }
+
+}
