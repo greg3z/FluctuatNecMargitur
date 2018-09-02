370684dd41170572ade3234089cc0cbc534551ac
Greg Lhotellier <glhotellier@gmail.com>
2018-09-02 11:43:38 -0400

    Add View target

---
+++ View/View.h
@@ -0,0 +1,19 @@
+//
+//  View.h
+//  View
+//
+//  Created by Grégoire Lhotellier on 02/09/2018.
+//  Copyright © 2018 Grégoire Lhotellier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for View.
+FOUNDATION_EXPORT double ViewVersionNumber;
+
+//! Project version string for View.
+FOUNDATION_EXPORT const unsigned char ViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <View/PublicHeader.h>
+
+
