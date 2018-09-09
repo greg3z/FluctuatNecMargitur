3aa8b22497a3001196e180bf6572b26b41ab3c4a
Greg Lhotellier <glhotellier@gmail.com>
2018-09-09 11:44:24 -0400

    StartCoordinator

---
+++ Coordinator/StartCoordinator.swift
@@ -0,0 +1,27 @@
+//
+//  StartCoordinator.swift
+//  Coordinator
+//
+//  Created by Grégoire Lhotellier on 09/09/2018.
+//  Copyright © 2018 Grégoire Lhotellier. All rights reserved.
+//
+
+import View
+
+public final class StartCoordinator: Coordinator {
+
+    public var startCallback: (() -> Void)?
+    public var rootViewController: UIViewController {
+        return startViewController
+    }
+    public var presentedCoordinator: Coordinator?
+    private let startViewController: StartViewController
+
+    public init() {
+        startViewController = StartViewController()
+        startViewController.buttonTapped = { [weak self] in
+            self?.startCallback?()
+        }
+    }
+
+}
