247a92052d8a447e96c6d03250d26b175bd6f9b1
Greg Lhotellier <glhotellier@gmail.com>
2018-09-09 14:55:39 -0400

    LogoutConfirmationCoordinator

---
+++ Coordinator/LogoutConfirmationCoordinator.swift
@@ -0,0 +1,27 @@
+//
+//  LogoutConfirmationCoordinator.swift
+//  Coordinator
+//
+//  Created by Grégoire Lhotellier on 09/09/2018.
+//  Copyright © 2018 Grégoire Lhotellier. All rights reserved.
+//
+
+import View
+
+public final class LogoutConfirmationCoordinator: Coordinator {
+
+    public var logoutCallback: (() -> Void)?
+    public var rootViewController: UIViewController {
+        return logoutConfirmationViewController
+    }
+    public var presentedCoordinator: Coordinator?
+    private let logoutConfirmationViewController: LogoutConfirmationViewController
+
+    public init() {
+        logoutConfirmationViewController = LogoutConfirmationViewController()
+        logoutConfirmationViewController.buttonTapped = { [weak self] in
+            self?.logoutCallback?()
+        }
+    }
+
+}
